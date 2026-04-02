5dc6d7e7e33a9479ddfa80a31254bfe567487926
christopher796 <ndaruchristopher100@gmail.com>
2026-04-02 22:34:14 +0300

    google verification

--- config/urls.py
+++ config/urls.py
@@ -32,7 +32,8 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
-]
+    path('google46505a87f900d8a9.html', auth_views.TemplateView.as_view(template_name='google46505a87f900d8a9.html'), name='google_verification'),
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
